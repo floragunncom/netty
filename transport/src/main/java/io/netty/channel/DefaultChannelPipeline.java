2427de113adc9f69a1b00fff386c009e694d9248
Norman Maurer <norman_maurer@apple.com>
2016-01-15 14:23:41 +0100

    [#4705] Ensure ChannelHandler.handlerAdded(...) is always called as first method of the handler
    Motivation:
    
    If a user adds a ChannelHandler from outside the EventLoop it is possible to get into the situation that handlerAdded(...) is scheduled on the EventLoop and so called after another methods of the ChannelHandler as the EventLoop may already be executing on this point in time.
    
    Modification:
    
    - Ensure we always check if the handlerAdded(...) method was called already and if not add the currently needed call to the EventLoop so it will be picked up after handlerAdded(...) was called. This works as if the handler is added to the ChannelPipeline from outside the EventLoop the actual handlerAdded(...) operation is scheduled on the EventLoop.
    - Some cleanup in the DefaultChannelPipeline
    
    Result:
    
    Correctly order of method executions of ChannelHandler.

--- transport/src/main/java/io/netty/channel/DefaultChannelPipeline.java
+++ transport/src/main/java/io/netty/channel/DefaultChannelPipeline.java
@@ -74,7 +74,9 @@ final class DefaultChannelPipeline implements ChannelPipeline {
         this.channel = channel;
 
         tail = new TailContext(this);
+        tail.setHandlerAddedCalled();
         head = new HeadContext(this);
+        head.setHandlerAddedCalled();
 
         head.next = tail;
         tail.prev = head;
@@ -102,7 +104,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
     }
 
     @Override
-    public ChannelPipeline addFirst(ChannelHandlerInvoker invoker, String name, ChannelHandler handler) {
+    public synchronized ChannelPipeline addFirst(ChannelHandlerInvoker invoker, String name, ChannelHandler handler) {
         name = filterName(name, handler);
         addFirst0(new DefaultChannelHandlerContext(this, invoker, name, handler));
         return this;
@@ -159,10 +161,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
     @Override
     public synchronized ChannelPipeline addBefore(
             EventExecutorGroup group, String baseName, String name, ChannelHandler handler) {
-        AbstractChannelHandlerContext ctx = getContextOrDie(baseName);
-        name = filterName(name, handler);
-        addBefore0(ctx, new DefaultChannelHandlerContext(this, findInvoker(group), name, handler));
-        return this;
+        return addBefore(findInvoker(group), baseName, name, handler);
     }
 
     @Override
@@ -193,10 +192,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
     @Override
     public synchronized ChannelPipeline addAfter(
             EventExecutorGroup group, String baseName, String name, ChannelHandler handler) {
-        AbstractChannelHandlerContext ctx = getContextOrDie(baseName);
-        name = filterName(name, handler);
-        addAfter0(ctx, new DefaultChannelHandlerContext(this, findInvoker(group), name, handler));
-        return this;
+        return addAfter(findInvoker(group), baseName, name, handler);
     }
 
     @Override
@@ -392,7 +388,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
         Future<?> future;
 
         synchronized (this) {
-            if (!ctx.channel().isRegistered() || ctx.executor().inEventLoop()) {
+            if (!isExecuteLater(ctx)) {
                 remove0(ctx);
                 return ctx;
             } else {
@@ -474,7 +470,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
             final AbstractChannelHandlerContext newCtx =
                     new DefaultChannelHandlerContext(this, ctx.invoker, newName, newHandler);
 
-            if (!newCtx.channel().isRegistered() || newCtx.executor().inEventLoop()) {
+            if (!isExecuteLater(newCtx)) {
                 replace0(ctx, newCtx);
                 return ctx.handler();
             } else {
@@ -537,7 +533,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
     }
 
     private void callHandlerAdded(final AbstractChannelHandlerContext ctx) {
-        if (ctx.channel().isRegistered() && !ctx.executor().inEventLoop()) {
+        if (isExecuteLater(ctx)) {
             ctx.executor().execute(new OneTimeTask() {
                 @Override
                 public void run() {
@@ -551,7 +547,12 @@ final class DefaultChannelPipeline implements ChannelPipeline {
 
     private void callHandlerAdded0(final AbstractChannelHandlerContext ctx) {
         try {
-            ctx.handler().handlerAdded(ctx);
+            try {
+                ctx.handler().handlerAdded(ctx);
+            } finally {
+                // handlerAdded(...) method was called.
+                ctx.setHandlerAddedCalled();
+            }
         } catch (Throwable t) {
             boolean removed = false;
             try {
@@ -576,7 +577,7 @@ final class DefaultChannelPipeline implements ChannelPipeline {
     }
 
     private void callHandlerRemoved(final AbstractChannelHandlerContext ctx) {
-        if (ctx.channel().isRegistered() && !ctx.executor().inEventLoop()) {
+        if (isExecuteLater(ctx)) {
             ctx.executor().execute(new OneTimeTask() {
                 @Override
                 public void run() {
@@ -597,6 +598,10 @@ final class DefaultChannelPipeline implements ChannelPipeline {
             fireExceptionCaught(new ChannelPipelineException(
                     ctx.handler().getClass().getName() + ".handlerRemoved() has thrown an exception.", t));
         }
+    }
+
+    private static boolean isExecuteLater(ChannelHandlerContext ctx) {
+        return ctx.channel().isRegistered() && !ctx.executor().inEventLoop();
     }
 
     /**
